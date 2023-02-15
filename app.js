739ad6e68b7e8a9056b27691b57063e058c5b4b5
Médéric Gbagba <medericjoel@gmail.com>
2023-02-15 00:15:13 +0000

    Vercel config

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
+
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.send('Hello World !')
+  res.sendFile('index.html', { root: path.join(__dirname, 'public') });
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+module.exports = app
